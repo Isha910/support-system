315aad81ab8543a10d05e1285935099eacb751ab
isha910 <isha.ishavyas@gmail.com>
2019-10-10 12:46:00 +0530

    wirte js for showing error if type and items attribute missing

--- js/script.js
+++ js/script.js
@@ -1,80 +1,154 @@
 var obj = [
-  {
-      "name": "Realtime Testing",
-      "type": "box",
-      "items": [
-          {
-              "name": "Category",
-              "type": "box",
-              "items": [
-                  {
-                      "name": "Mobile",
-                      "type": "box",
-                      "items": [
-                          {
-                              "name":"Android",
-                              "items":[]
-                          }
-                      ]
-                  },
-                  {
-                      "name": "Browser",
-                      "items": []
-                  },
-                  {
-                      "name": "Desktop",
-                      "items": []
-                  }
-              ]
-          },
-          {
-              "name": "Questions"
-          }
-      ]
-  }, {
-      "name": "Automation Testing",
-      "type": "box",
-      "items": [
-          {
-              "name": "Category",
-              "type": "box",
-              "items": [
-                  {
-                      "name": "Mobile",
-                      "items": []
-                  },
-                  {
-                      "name": "Browser",
-                      "items": []
-                  },
-                  {
-                      "name": "Desktop",
-                      "items": []
-                  }
-              ]
-          },
-          {
-              "name": "Questions"
-          }
-      ]
-  }
+    {
+        "name": "Realtime Testing",
+        "type": "box",
+        "items": [
+            {
+                "name": "Category",
+                "type": "box",
+                "items": [
+                    {
+                        "name": "Mobile",
+                        "type": "box",
+                        "items": [
+                            {
+                                "name": "Android",
+                                "type": "box",
+                                "items": []
+                            }
+                        ]
+                    },
+                    {
+                        "name": "Browser",
+                        "items": []
+                    },
+                    {
+                        "name": "Desktop",
+                        "items": []
+                    }
+                ]
+            },
+            {
+                "name": "Questions",
+                "type": "ques",
+                "items": []
+            }
+        ]
+    }, {
+        "name": "Automation Testing",
+        "type": "box",
+        "items": [
+            {
+                "name": "Category",
+                "type": "box",
+                "items": [
+                    {
+                        "name": "Mobile",
+                        "items": []
+                    },
+                    {
+                        "name": "Browser",
+                        "items": []
+                    },
+                    {
+                        "name": "Desktop",
+                        "items": []
+                    }
+                ]
+            },
+            {
+                "name": "Questions"
+            }
+        ]
+    }
 ];
 
 var parsedObj = JSON.stringify(obj);
 var newObj = JSON.parse(parsedObj);
 
-function runLoop() {
-    
-    for(let i = 0; i<newObj.length; i++) {
-      
-      if(newObj[i].type == "") {
-        console.log('Cant print this object'); 
-        continue;
-      }
-      console.log(newObj[i].items,newObj[i].name, newObj[i].type);
-      
+
+
+var i = 0;
+var html = [];
+
+function createList(arr) {
+
+    if (i == 0) {
+        html.push('<ul>');
+    } else {
+        html.push('<ul class="nested">');
     }
-   }
-runLoop();
+    i++;
+    $.each(arr, function (i, val) {
+        if (!val.type) {
+            console.error("error type not found in", val.name)
+        }
+        else if (val.type === "box") {
+            console.log(" box type found", val.name)
+            html.push('<li><span class="caret">' + val.name + '</span>');
+
+        } else if (val.type === "ques") {
+            console.log("ques type found", val.name)
+            html.push('<li><span class="type">' + val.name + '</span>');
+        }
+
+        else {
+            html.push('<li>' + val.name);
+        }
+
+
+        if (val.items) {
+            createList(val.items)
+        }
+
+        html.push('</li>');
+    });
+    html.push('</ul>');
+
+
+}
+
+
+createList(obj);
+
+
+
+// console.log('',obj)
+// // console.log(obj[0].items[1])
+// const key = "type";
+// const value= "box";
+// const result = obj[0].items.filter(d=>d[key]==value);
+
+// console.log('my rw', result);
+
+// function getData(...params) {
+//     console.log("**********")
+//     // console.log(params[0]);
+//     // console.log(obj[params[0].items[params[1]]])
+//     // console.log(newObj)
+//     var selector = '';
+//     var temp;
+//     for (i= 1; i < params.length; i++){
+//         // obj[0].items[0].items.filter(d=>d[key]==value);
+//         //console.log(params[i]);
+
+
+//         selector += `[${params[i]}].items`
+//          //console.log({selector});
+         
+         
+       
+//     }
+    
+    // console.log(selector)
+
+    // y.filter(d=>d[key]==value);
+
+//     var seletedArr = 'obj'+`${selector}`
+
+
+
+
+
 
 
