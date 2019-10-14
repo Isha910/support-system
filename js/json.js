e7916dcd5d4a3964cd396ec98c2367ac60760a57
isha910 <isha.ishavyas@gmail.com>
2019-10-14 14:22:29 +0530

    add json data from script.js file

---
+++ js/json.js
@@ -0,0 +1,65 @@
+var obj = [
+      {
+          "name": "Realtime Testing",
+          "type": "box",
+          "items": [
+              {
+                  "name": "Category",
+                  "type": "box",
+                  "items": [
+                      {
+                          "name": "Mobile",
+                          "type": "box",
+                          "items": [
+                              {
+                                  "name": "Android",
+                                  "type": "box",
+                                  "items": []
+                              }
+                          ]
+                      },
+                      {
+                          "name": "Browser",
+                          "items": []
+                      },
+                      {
+                          "name": "Desktop",
+                          "items": []
+                      }
+                  ]
+              },
+              {
+                  "name": "Questions",
+                  "type": "ques",
+                  "items": []
+              }
+          ]
+      }, {
+          "name": "Automation Testing",
+          "type": "box",
+          "items": [
+              {
+                  "name": "Category",
+                  "type": "box",
+                  "items": [
+                      {
+                          "name": "Mobile",
+                          "items": []
+                      },
+                      {
+                          "name": "Browser",
+                          "items": []
+                      },
+                      {
+                          "name": "Desktop",
+                          "items": []
+                      }
+                  ]
+              },
+              {
+                  "name": "Questions"
+              }
+          ]
+      }
+  ];
+  
